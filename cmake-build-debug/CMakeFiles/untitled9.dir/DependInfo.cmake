18fbe7686abb48eecb60804b73b36dad752bb830
nzolotoff <nikita.zolotov37@yandex.ru>
2023-02-21 02:00:24 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/untitled9.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nzolotoff/CLionProjects/untitled9/main.cpp" "CMakeFiles/untitled9.dir/main.cpp.o" "gcc" "CMakeFiles/untitled9.dir/main.cpp.o.d"
+  "/Users/nzolotoff/CLionProjects/untitled9/task6.cpp" "CMakeFiles/untitled9.dir/task6.cpp.o" "gcc" "CMakeFiles/untitled9.dir/task6.cpp.o.d"
+  "/Users/nzolotoff/CLionProjects/untitled9/task 4.cpp" "CMakeFiles/untitled9.dir/task_4.cpp.o" "gcc" "CMakeFiles/untitled9.dir/task_4.cpp.o.d"
+  "/Users/nzolotoff/CLionProjects/untitled9/task 5.cpp" "CMakeFiles/untitled9.dir/task_5.cpp.o" "gcc" "CMakeFiles/untitled9.dir/task_5.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/untitled9.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled9.dir/main.cpp.o"
+  "CMakeFiles/untitled9.dir/main.cpp.o.d"
+  "CMakeFiles/untitled9.dir/task6.cpp.o"
+  "CMakeFiles/untitled9.dir/task6.cpp.o.d"
+  "CMakeFiles/untitled9.dir/task_4.cpp.o"
+  "CMakeFiles/untitled9.dir/task_4.cpp.o.d"
+  "CMakeFiles/untitled9.dir/task_5.cpp.o"
+  "CMakeFiles/untitled9.dir/task_5.cpp.o.d"
+  "untitled9"
+  "untitled9.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
