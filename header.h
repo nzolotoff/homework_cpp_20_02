36051dba926feb08f1077943e36b1a1316da70ba
nzolotoff <nikita.zolotov37@yandex.ru>
2023-02-21 08:17:41 +0300

    initial commit

--- header.h
+++ header.h
@@ -10,5 +10,9 @@
 template <typename Type>
 std::vector<Type> replacingOddEven(std::vector<Type>&vector, std::list<Type>&list);
 
+template<typename T>
+std::list<T> alternate_pairs(const std::vector<T>& vec, const std::list<T>& lst);
+
 void task4();
 void task5();
+void task6();
